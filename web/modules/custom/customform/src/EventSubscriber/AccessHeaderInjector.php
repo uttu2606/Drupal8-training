b9e6fb6bf139219a8b8a315597947e87bb6453ad
uttu2606 <uttkarsh.tiwari@capgemini.com>
2017-05-08 18:36:25 +0530

    latest

--- web/modules/custom/customform/src/EventSubscriber/AccessHeaderInjector.php
+++ web/modules/custom/customform/src/EventSubscriber/AccessHeaderInjector.php
@@ -1,1 +1,19 @@
 <?php
+ namespace Drupal\customform\EventSubscriber;
+
+ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+ use Symfony\Component\HttpKernel\KernelEvents;
+ use Symfony\Component\HttpKernel\Event\FilterResponseEvent;
+
+ class AccessHeaderInjector implements EventSubscriberInterface {
+
+ 	public static function getSubscribedEvents() {
+ 		return [
+ 		  KernelEvents::RESPONSE => ['addAccessHeader']
+ 		  ];
+ 	}
+ 	public function addAccessHeader(FilterResponseEvent $event) {
+ 		$response = $event->getResponse();
+ 		$response->headers->add(['access-control-allow-origin:' => '*']);
+ 	}
+ }
